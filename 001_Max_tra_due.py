79400835f3f2989f379a895acb9eff87a6bab3ab
KorraDH <korra@corradobigliardi.it>
2023-12-10 19:34:49 +0100

    Agguinta di 001_Max_tra_due

---
+++ 001_Max_tra_due.py
@@ -0,0 +1,8 @@
+print('Inserisci due numeri, ti dirò il più grande')
+x = input('Primo numero: ')
+y = input('Secondo numero: ')
+
+if x > y :
+    print('il numero più grande è: ' + str(x))
+else :
+    print('il numero più grande è: ' + str(y))
