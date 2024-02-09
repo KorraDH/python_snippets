d00b9fa0520adaf8a0536097a0349c74ff273f06
KorraDH <korra@corradobigliardi.it>
2024-02-09 22:06:15 +0100

    Versione 0.1.2 di GESVER

--- ANFGCO/GESVER.py
+++ ANFGCO/GESVER.py
@@ -2,13 +2,16 @@ import tkinter as tk
 from tkinter import ttk
 from tkinter import filedialog
 
-versione = 'versione 0.1 del 09-02-2024'
+versione = 'versione 0.1.2 del 09-02-2024'
 
 def importa_file():
     window_importa = tk.Toplevel(root)
     window_importa.title("Importa file da terminalino")
     window_importa.geometry('640x480')
+    window_importa.lift #la tengo in primo piano
     
+    filepath = ''
+
     def seleziona_file():
         # definisco le estensioni possibili
         file_types = [("File di testo", "*.txt *.csv")]
@@ -16,20 +19,35 @@ def importa_file():
         # chiedo di selezionare un file con le estensioni possibili
         filename = filedialog.askopenfilename(title='Seleziona un file', initialdir="/home", filetypes=file_types)
         
-        # apro il file in lettura, metto i dati nella variabile data e li stampo
-        f = open(filename, 'r')
-        data = f.read()
+        # aggiorno la entry con il percorso
         ent_nomefile.delete(0, tk.END)
         ent_nomefile.insert(0, filename)
-        
-    lbl_ins_path = tk.Label(text="Indica o seleziona il percorso del file")
+    
+    def importa_file():
+        global data # dichiarando la variabile come globale posso utilizzarla anche fuori dalla funzione
+        filepath = ent_nomefile.get()
+        # testo se filepath è vuoto
+        if not filepath:
+            return
+            
+        #print('Inizio importazione dati')
+        f = open(filepath, 'r')
+        data = f.read()
+        #print('Fine importazione dati') 
+        fine = tk.Label(window_importa, text='Importazione terminata!', bg='green')
+        fine.grid(column=0, row=3)
+                        
+    lbl_ins_path = tk.Label(window_importa, text="Indica o seleziona il percorso del file")
     ent_nomefile = tk.Entry(window_importa, width=50)
-    btn_importa = tk.Button(window_importa, text='Apri file', command=seleziona_file)
+    btn_selezionafile = tk.Button(window_importa, text='Apri file', command=seleziona_file)
+    btn_importa = tk.Button(window_importa, text='Importa dati', bg='orange', borderwidth=4, command=importa_file)
+    btn_stampa = tk.Button(window_importa, text='Stampa dati', command=lambda: print(data))
     
     lbl_ins_path.grid(column=0, row=0, padx=5, pady=5)
     ent_nomefile.grid(column=0, row=1, padx=5, pady=5)
-    btn_importa.grid(column=1, row=1, padx=5, pady=5)
-    
+    btn_selezionafile.grid(column=1, row=1, padx=5, pady=5)
+    btn_importa.grid(column=0, row=2, padx=5, pady=5)
+    btn_stampa.grid(column=1, row=2, padx=5, pady=5)
 
     # bind per uscire dalla maschera
     def on_key_press(event):
@@ -45,7 +63,7 @@ def help():
     lbl_immagine = tk.Label(window_help, image=ico_programma)
     lbl_titolo = tk.Label(window_help, text="Gestione verifiche capoturni")
     lbl_versione = tk.Label(window_help, text=versione)
-    lbl_crediti = tk.Label(window_help, text="Copyright Corrado Bigliardi\n Iter Innovation")
+    lbl_crediti = tk.Label(window_help, text="Copyright Corrado Bigliardi\nIter Innovation\nper Anofor Srl")
 
     lbl_immagine.grid(column=0, row=0 ,padx=5, pady=20)
     lbl_titolo.grid(column=0, row=1, padx=5, pady=5)
@@ -83,13 +101,21 @@ menubar = tk.Menu(root)
 root.config(menu=menubar)
 # poi creo le voce di menù
 menu_file = tk.Menu(menubar, tearoff=0) # con il parametro tearoff=0 elimino la prima riga in alto
-menu_help = tk.Menu(menubar)
+menu_help = tk.Menu(menubar, tearoff=0)
 
 # assegno alle voci al menu
 menubar.add_cascade(label='File', menu=menu_file)
 menubar.add_cascade(label='Help', menu=menu_help)
 
 # creo i comandi sotto alle voci di menu
+menu_anagrafiche = tk.Menu(menu_file, tearoff=0)
+menu_file.add_cascade(label='Anagrafiche', menu=menu_anagrafiche)
+menu_anagrafiche.add_command(label='Stabilimenti', command='')
+menu_anagrafiche.add_command(label='Turni', command='')
+menu_anagrafiche.add_command(label='Controlli', command='')
+menu_anagrafiche.add_command(label='Capiturno', command='')
+
+menu_file.add_separator()
 menu_file.add_command(label='Importa file da terminalino', command=importa_file)
 menu_file.add_separator()
 menu_file.add_command(label='Esci', command=root.quit)
